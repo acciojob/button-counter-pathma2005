0105f84786c1d0291d8f1b32eab519b34acb68d8
Pathmasri E <pathmasri1372005@gmail.com>
2025-12-01 18:11:36 +0530

    Fix button click message formatting

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <p>Button Clicked{count}times</p>
+    <button onClick={() => setCount(count + 1)}>Click Me</button>
     </div>
   )
 }
